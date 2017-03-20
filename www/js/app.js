52fb0606e1bcdef8990b8ec792a869652cb034d0
santyjar <santy.1.82@hotmail.com>
2017-03-20 09:56:48 -0300

    modificado: www/css/estilos.css 	modificado: www/js/app.js 	modificado: www/kiosco.sublime-workspace 	new file:   www/template/alumnoData.template.html 	modificado: www/template/main.template.html

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,68 @@
-angular.module('kioscoApp',['ngRoute']);
+'use strict';
+
+angular.module('kioscoApp',['ngRoute','main']);
 
 
 //Modulo kiosco y rutas
 angular
-	.module('kioscoApp')
-	.config(['$locationProvider','$routeProvider',
-		function config($locationProvider,$routeProvider){
-			$routeProvider.
-			when('/',{
-				template: '<main></main>'
-			});
-		}
-	]);
+.module('kioscoApp')
+.config(['$locationProvider','$routeProvider',
+	function config($locationProvider,$routeProvider){
+		$routeProvider.
+		when('/',{
+			template: '<main></main>'
+		});
+	}
+]);
+
+//Modulo main
+
+angular.module('main',['alumno']);
 
 angular
-	.module('kioscoApp')
-	.component('main',{
-		templateUrl:'template/main.template.html',
-		controller:function mainController(){
-			this.dni = '';
+.module('main')
+.component('main',{
+	templateUrl:'template/main.template.html',
+
+	controller:['$http',function mainController($http){
+		var self = this;
+		self.dni = '';
+
+		self.getAlumno = function(){
+			$http({
+				url:'http://localhost/kiosco_php/ajax/alumno_seleccionar.php',
+				method: 'GET',
+				params:{alumno: {dni:self.dni}}
+			}).then(function(response) {
+				if(response.data.resp == true){
+					self.alumno.data = response.data
+				}else{
+
+				}
+				console.log(alumno);
+			});
 		}
-	});
+	}]
+});
+
+//Modulo alumno
+angular.module('alumno',['ngResource']);
+
+angular
+.module('alumno')
+.component('alumno',{
+	templateUrl:'template/alumnoData.template.html',
+	controller:function alumnoController(){
+		this.data = {
+			matricula:false,
+			dni:false,
+			nombre:false,
+			apellido:false,
+			carrera:false,
+			asistencia:false,
+			cursada:false,
+			parciales:false,
+			tps:false
+		}
+	}
+});
