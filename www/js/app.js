88158654a698f75c2c5ed288bb91a3dbf2671474
santyjar <santy.1.82@hotmail.com>
2017-03-22 15:23:10 -0300

    modificado: www/js/app.js 	modificado: www/kiosco.sublime-workspace 	new file:   www/template/alumno.template.html 	new file:   www/template/dni.template.html 	modificado: www/template/main.template.html
    	deleted:    www/template/alumnoData.template.html

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
-angular.module('kioscoApp',['ngRoute','main']);
-
+angular.module('kioscoApp',['ngRoute','main','dni','alumno']);
 
 //Modulo kiosco y rutas
 angular
@@ -13,22 +12,89 @@ angular
 			template: '<main></main>'
 		});
 	}
-]);
+	]);
+
+
 
 //Modulo main
-
-angular.module('main',['alumno']);
+//Trabajar con servicios para conseguir el alumno
+angular.module('main',['dni','alumno']);
 
 angular
 .module('main')
 .component('main',{
 	templateUrl:'template/main.template.html',
 
-	controller:['$http',function mainController($http){
-		var self = this;
-		self.dni = '';
+	controller:function mainController($http){}
+});
 
-		self.getAlumno = function(){
+
+
+
+//Modulo alumno
+angular.module('dni',['alumno']);
+
+angular
+.module('dni')
+.component('dni',{
+	templateUrl:'template/dni.template.html',
+	controller:['factAlumno',
+	function dniController(factAlumno){
+		this.dni = false;
+		this.alumno = false;
+		
+		this.getAlumno = function(){
+			this.alumno = factAlumno.getAlumno(this.dni);
+		}
+
+		this.setAlumno = function(prop,dato){
+			factAlumno.set(prop,dato);
+		}
+
+		this.getAlumno();
+	}]
+});
+
+
+//Modulo alumno
+angular.module('alumno',['ngResource']);
+
+angular
+.module('alumno')
+.component('alumno',{
+	templateUrl:'template/alumno.template.html',
+	controller:function alumnoController(){
+		
+	}
+});
+
+//Factory para obtener un alumno
+
+angular.module('alumno')
+.factory('factAlumno',['$http',function($http){
+	this.data = {
+		matricula:false,
+		dni:false,
+		nombre:false,
+		apellido:false,
+		carrera:false,
+		asistencia:false,
+		cursada:false,
+		parciales:false,
+		tps:false
+	}
+
+
+	return {
+		getAlumno: function(dni){
+			console.log(this.data;
+		},
+		set: function(prop,dato){
+			//this.data[prop] = dato;
+			console.log(this);
+		}
+
+		/*function(dni){
 			$http({
 				url:'http://localhost/kiosco_php/ajax/alumno_seleccionar.php',
 				method: 'GET',
@@ -39,30 +105,8 @@ angular
 				}else{
 
 				}
-				console.log(alumno);
 			});
-		}
-	}]
-});
-
-//Modulo alumno
-angular.module('alumno',['ngResource']);
-
-angular
-.module('alumno')
-.component('alumno',{
-	templateUrl:'template/alumnoData.template.html',
-	controller:function alumnoController(){
-		this.data = {
-			matricula:false,
-			dni:false,
-			nombre:false,
-			apellido:false,
-			carrera:false,
-			asistencia:false,
-			cursada:false,
-			parciales:false,
-			tps:false
-		}
+			return self.alumno;
+		}*/
 	}
-});
+}]);
